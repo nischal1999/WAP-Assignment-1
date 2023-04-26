b3d4df1dddd2951df4d4b952c336469627aea059
nischal1999 <95971668+nischal1999@users.noreply.github.com>
2023-04-25 22:00:17 -0500

    Update add.js
    Minor refactoring

--- Lab14/shopping-cart_frontend_demo/js/add.js
+++ Lab14/shopping-cart_frontend_demo/js/add.js
@@ -5,25 +5,17 @@ if(product!=null){
     document.getElementById('description').value = product.description;
     document.getElementById('price').value = product.price;
     document.getElementById('id').value = product.id;
-    
     document.getElementById('header-text').innerHTML = "Edit Product";
     document.getElementById('btnRegister').innerHTML = "Save";
-    
     localStorage.removeItem("product");
 }
 
 
-async function postProduct(title, description, price) {
-    let b = { "title": title, "description": description, "price": price }
-    let setting = {
-        method: 'POST',
-        body: JSON.stringify(b),
-        headers: { 'Content-Type': 'application/json' }
-    };
-    const response = await fetch("http://localhost:3000/products", setting);
-    const jsonData = await response.json();
-    return jsonData;
-}
+
+
+
+
+
 
 async function update(id, title, description, price) {
     let b = { "title": title, "description": description, "price": price }
@@ -38,23 +30,41 @@ async function update(id, title, description, price) {
 }
 
 
+
+
+
+
+async function postProduct(title, description, price) {
+    let b = { "title": title, "description": description, "price": price }
+    let setting = {
+        method: 'POST',
+        body: JSON.stringify(b),
+        headers: { 'Content-Type': 'application/json' }
+    };
+    const response = await fetch("http://localhost:3000/products", setting);
+    const jsonData = await response.json();
+    return jsonData;
+}
+
+
+
+
+
+
+
 document.getElementById('btnRegister').addEventListener('click', (event) => {
     const title = document.getElementById('title').value;
     const description = document.getElementById('description').value;
     const price = document.getElementById('price').value;
     const id = document.getElementById('id').value;
-    
-    if(id==null || id.trim()==""){
+    if(id == null || id.trim() == ""){
         data = postProduct(title, description, price);
-
         document.getElementById('title').innerHTML = data;
         document.getElementById('myform').reset();
     }
     else {
         data = update(id, title, description, price);
-        document.getElementById('title').innerHTML = data;
-        
+        document.getElementById('title').innerHTML = data; 
         location.href = "index.html";
-    }
-   
+    }   
 });
